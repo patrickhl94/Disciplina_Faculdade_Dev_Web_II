29ab21cd72c390ccd6c8d3f1e38cc775112a6482
Patrick Lima <patrickhl94@hotmail.com>
2020-05-13 23:46:01 -0300

    Created styles component input

---
+++ front_app_coronavirus_react/src/components/Input/styles.ts
@@ -0,0 +1,54 @@
+import styled, { css } from 'styled-components';
+
+interface InputProps {
+  widthStyle: 'max' | 'min';
+}
+
+export const Label = styled.label<InputProps>`
+  display: flex;
+  flex-direction: column;
+  color: #666;
+  font-weight: 700;
+  border-bottom: 5px;
+  
+${props =>
+  props.widthStyle === 'max'
+    ? css`
+        width: 100%;
+      `
+    : css`
+        width: 48%;
+      `};
+}
+  input {
+    color: #111;
+    font-size: 18px;
+    height: 45px;
+    border-radius: 8px;
+    border: 0;
+    padding: 10px 10px;
+    background: #6f2725;
+    margin-bottom: 10px;
+    margin-top: 5px;
+`;
+
+export const Select = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #666;
+  font-weight: 700;
+  border-bottom: 5px;
+  width: 48%;
+
+  select {
+    color: #111;
+    font-size: 18px;
+    height: 45px;
+    border-radius: 8px;
+    border: 0;
+    padding: 10px 10px;
+    background: #6f2725;
+    margin-bottom: 10px;
+    margin-top: 5px;
+  }
+`;
