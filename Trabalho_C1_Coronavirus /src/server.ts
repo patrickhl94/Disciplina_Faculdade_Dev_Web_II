cc8a65c44d29f8546b35c0740a745d2da973a025
Patrick Lima <patrickhl94@hotmail.com>
2020-05-14 22:36:01 -0300

    Add cors

--- Trabalho_C1_Coronavirus /src/server.ts
+++ Trabalho_C1_Coronavirus /src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import './database';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
